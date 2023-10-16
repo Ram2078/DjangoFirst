60a1940fed546fdfe753e31c00b169ea28ad33ff
Ram2078 <ramil.nasybullin1999@mail.ru>
2023-10-16 21:43:12 +0300

    Добавлены новые страницы на сайт.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# DjangoFirst
+# Обычный сайт
+
+Сайт со стандартными страницами
+
+## Установка
+
+1. Склонируйте репозиторий:
+
+   ```shell
+   git clone https://github.com/Ram2078/DjangoFirst
+   ```
+2. Создайте виртуальное окружение и активируйте его:
+
+   ```shell
+   python -m venv venv
+   source venv/bin/activate
+   ```
+3. Установите зависимости:
+
+   ```shell
+   pip install -r requirements.txt
+   ```
+4. Примените миграции:
+
+   ```shell
+   python manage.py migrate
+   ```
+
+5. Запуск приложения:
+
+   ```shell
+   python manage.py runserver
+   ```
+
--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Загрузка переменных окружения из файла .env
+load_dotenv()
+
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+DATABASE_USER = os.getenv('DATABASE_USER')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
