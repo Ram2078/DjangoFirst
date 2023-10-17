0f2d7ec1d46b45ebae182ddcb0b253a867a7cd69
Ram2078 <ramil.nasybullin1999@mail.ru>
2023-10-17 20:39:16 +0300

    Правка файлов под pep8

--- lyceum/homepage/views.py
+++ lyceum/homepage/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Вы на домашней странице. Вы можете перейти на 'catalog/'"
-                        " или 'about/'")
+    return HttpResponse("Вы на домашней странице. Вы можете перейти на "
+                        "'catalog/' или 'about/'")
